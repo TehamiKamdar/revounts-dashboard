4e1d4f8f5bb89407cd9a4c7cfe8777d994305724
TehamiKamdar <tehamikamdar15@gmail.com>
2025-09-23 18:21:38 +0500

    Auth Left

--- storage/framework/views/2f550a68a3fc7743a7746add84ed0d5f56431155.php
+++ storage/framework/views/2f550a68a3fc7743a7746add84ed0d5f56431155.php
@@ -1,6 +1,4 @@
-@extends("auth.publisher_register.base")
-
-@section("step_form_content")
+<?php $__env->startSection("step_form_content"); ?>
 
     <div class="card checkout-shipping-form" style="padding-bottom: 20px;">
         <div class="card-header border-bottom-0 align-content-start pb-sm-0 pb-1 px-0">
@@ -14,28 +12,28 @@
                         <div>
                             <label for="firstName">First Name*</label>
                             <input type="text" class="form-control" id="first_name" name="first_name"
-                                    placeholder="First Name" value="{{ $stepOne['first_name'] ?? null }}">
+                                    placeholder="First Name" value="<?php echo e($stepOne['first_name'] ?? null); ?>">
                         </div>
                         <div>
                             <label for="lastName">Last Name*</label>
                             <input type="text" class="form-control" id="last_name" name="last_name"
-                                    placeholder="Last Name" value="{{ $stepOne['last_name'] ?? null }}">
+                                    placeholder="Last Name" value="<?php echo e($stepOne['last_name'] ?? null); ?>">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="username">Username*</label>
                         <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Username"
-                            value="{{ $stepOne['user_name'] ?? null }}">
+                            value="<?php echo e($stepOne['user_name'] ?? null); ?>">
                     </div>
                     <div class="form-group">
                         <label for="email">Email Address*</label>
                         <input type="email" class="form-control" id="email" name="email" placeholder="username@email.com"
-                            value="{{ $stepOne['email'] ?? null }}">
+                            value="<?php echo e($stepOne['email'] ?? null); ?>">
                     </div>
                     <div class="form-group">
                         <label for="password">Password*</label>
                         <input id="password" class="form-control" type="password" name="password" required
-                                autocomplete="current-password" value="{{ $stepOne['password'] ?? null }}">
+                                autocomplete="current-password" value="<?php echo e($stepOne['password'] ?? null); ?>">
                             <div class="fa fa-fw fa-eye-slash text-light fs-16 field-icon" id="password-icon"
                                 onclick="showPassword('password')"></div>
                     </div>
@@ -43,12 +41,12 @@
                         <label for="confirmPassword">Confirm Password*</label>
                         <input id="password_confirmation" class="form-control" type="password"
                                 name="password_confirmation" required autocomplete="current-password_confirmation"
-                                value="{{ $stepOne['password_confirmation'] ?? null }}">
+                                value="<?php echo e($stepOne['password_confirmation'] ?? null); ?>">
                             <div class="fa fa-fw fa-eye-slash text-light fs-16 field-icon" id="password_confirmation-icon"
                                 onclick="showPassword('password_confirmation')"></div>
                     </div>
                     <div class="checkbox-group">
-                        <input type="checkbox" class="checkbox" id="agree" name="agree" value="1" {{ isset($stepOne['agree']) && $stepOne['agree'] ? "checked" : null }} />
+                        <input type="checkbox" class="checkbox" id="agree" name="agree" value="1" <?php echo e(isset($stepOne['agree']) && $stepOne['agree'] ? "checked" : null); ?> />
                         <label for="terms">I Agree With The <a href="https://www.linkscircle.com/terms">Terms And Conditions.</a></label>
                     </div>
                     <div class="form-buttons right-align">
@@ -59,4 +57,5 @@
         </div>
     </div><!-- ends: card -->
 
-@endsection
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make("auth.publisher_register.base", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\lenovo\Desktop\revdb\resources\views/auth/publisher_register/step_one.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d848c315491cda3386187f2dd83fb6f2cec955ed.php
@@ -0,0 +1,22 @@
+<div class="checkout-progress justify-content-center px-0">
+    <div class="step current" id="1">
+        <span>1</span>
+    </div>
+    <div class="step" id="2">
+        <span>2</span>
+    </div>
+    <div class="step" id="3">
+        <span>3</span>
+    </div>
+    <div class="step" id="4">
+        <span>4</span>
+    </div>
+</div>
+<div class="card-header border-bottom-0">
+    <h1 class="pt-20">Join as a Publisher</h1>
+    <p>Start registering your publisher account with LinksCircle and partner with advertisers.</p>
+    <div class="div">
+        <img class="svg w-100" src="<?php echo e(\App\Helper\Static\Methods::staticAsset("img/svg/progress1.svg")); ?>" alt="img">
+    </div>
+</div>
+<?php /**PATH C:\Users\lenovo\Desktop\revdb\resources\views/auth/publisher_register/steps/one.blade.php ENDPATH**/ ?>
